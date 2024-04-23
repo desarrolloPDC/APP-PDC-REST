80ff53a9a69739fcbf4e4a058b1f2e349f5fefa4
desarrolloPDC <7500@parquedelacosta.com.ar>
2024-04-23 16:08:44 -0300

    se modifico el index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require('./src/routes/routes');
+const https = require('https')
+const path = require('path')
+const fs = require('fs')
 
 //crear servidor de node
 const app = express();
@@ -17,6 +20,18 @@ app.use(express.urlencoded({extended:true})); //form-urlencoded
 app.use('/', routes)
 
 //crear server y escuchar petisiones
-app.listen(3000, () => {
-    console.log("Server is now running on port " + 3000)
-})
+// app.listen(3000, () => {
+//     console.log("Server is now running on port " + 3000)
+// })
+
+const sslServer = https.createServer(
+    {
+        key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+    }, 
+    app
+)
+
+sslServer.listen(3443, 
+    () => console.log('Server is now running on port 3443')
+)
